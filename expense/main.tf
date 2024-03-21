cb40a00f08eab12845c5c90f31f93112bc998821
sailalith <lalithvoop@hotmail.com>
2024-03-22 00:38:54 +0530

    added expense - for each loop

---
+++ expense/main.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "vms-1" {
+    for_each = var.instance_type
+    ami = var.ami-value
+    vpc_security_group_ids =  var.vpc-group-id
+    instance_type = each.value[instance_types]
+    tags = {
+        Name = each.key
+    }
+
+}
+
+variable "ami-value" {
+    default = "ami-05f020f5935e52dc4"
+}
+
+variable "vpc-group-id" {
+    default = ["sg-01ab91863562a96bf"]
+}
+
+variable "instance_type" {
+    default = {
+        frontend = {
+            instance_types = "t3.micro"
+        }
+        mysql = {
+            instance_types = "t3.small"
+        }
+        backend = {
+            instance_types = "t3.micro"
+        }
+    }
+}
