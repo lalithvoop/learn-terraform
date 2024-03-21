99c1115df572fbf37254e7e937d409d694dc2107
sailalith <lalithvoop@hotmail.com>
2024-03-21 23:02:22 +0530

    added tags

--- 03-create_expense/main.tf
+++ 03-create_expense/main.tf
@@ -1,16 +1,28 @@
 resource "aws_instance" "sample"{
     ami = var.ami-value
     instance_type = var.instance-type
+
+    tags = {
+        Name = frontend
+    }
 }
 
 resource "aws_instance" "sample2"{
     ami = var.ami-value
     instance_type = var.instance-type
+
+    tags = {
+        Name = frontend
+    }
 }
 
 resource "aws_instance" "sample3"{
     ami = var.ami-value
     instance_type = var.instance-type
+
+    tags = {
+        Name = frontend
+    }
 }
 
 variable "ami-value" {
