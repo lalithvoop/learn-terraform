d4a8347f6891ebf0b6481c81b0e51749748e71ed
sailalith <lalithvoop@hotmail.com>
2024-03-21 23:11:35 +0530

    added vars.tf and main.tfvars

---
+++ 03-create_expense/main.tfvars
@@ -0,0 +1,3 @@
+vpc-group-id = ["sg-01ab91863562a96bf"]
+instance-type = "t3.micro"
+ami-value = "ami-05f020f5935e52dc4"
---
+++ 03-create_expense/vars.tf
@@ -0,0 +1,11 @@
+variable "ami-value" {
+    default = "ami-05f020f5935e52dc4"
+}
+
+variable "instance-type" {
+    default = "t3.micro"
+}
+
+variable "vpc-group-id" {
+    default = ["sg-01ab91863562a96bf"]
+}
